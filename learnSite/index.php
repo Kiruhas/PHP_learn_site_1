d06a460b1283d44382d23376217347e0745edb1e
kiruhas <kiruhas46@gmail.com>
2020-06-02 23:09:13 +0300

    first

--- learnSite/index.php
+++ learnSite/index.php
@@ -16,6 +16,11 @@
             <h2 class="logo">Новости</h2>
             <div class="header_list">
                 <a class="list_btn" href="new_article.php">Добавить статью</a>
+                <?php if ($_COOKIE['user'] == 'Да'): ?>
+                    <a class="list_btn" href="auth.php">Кабинет</a>
+                <?php else: ?>
+                    <a class="list_btn" href="auth.php">Войти</a>
+                <?php endif ?>
             </div>
         </header>
     </div>
@@ -30,6 +35,7 @@
 
                 $query_text = 'SELECT * FROM "public"."Новости" ORDER BY id';
                 $query = pg_query($db_connection, $query_text);
+                echo '<div>' . $_COOKIE['user'] . '</div>';
                 while ($res = pg_fetch_object($query)):
             ?>
                 <div class="states_item">
